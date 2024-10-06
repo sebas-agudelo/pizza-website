48771aff809d5a6457f9764ecfd14c1764b1f28a
Sebastian Agudelo <sebastianagudelo95@hotmail.com>
2024-10-07 01:39:18 +0200

    Added supabase

--- my-express-app/api/index.ts
+++ my-express-app/api/index.ts
@@ -1,11 +1,32 @@
 import express from "express";
+import cors from 'cors'
+import dotenv from 'dotenv';
+import { createClient } from '@supabase/supabase-js'
+
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Express on Vercell");
+app.use(express.json());
+app.use(cors());
+
+dotenv.config();
+
+const supabaseUrl = process.env.SUPABASE_URL || '';
+const supabaseKey = process.env.SUPABSE_KEY || '';
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+
+
+app.get("/", async (req, res) => {
+    
+let { data: Products, error } = await supabase
+.from('Products')
+.select('*')
+
+
+
+console.log(Products);
 
 })     
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
-module.exports = app;
