7111637933000c8ca8493cc31af3da9150059ea0
Sebastian Agudelo <sebastianagudelo95@hotmail.com>
2024-10-07 02:07:08 +0200

    cors origin

--- my-express-app/api/index.ts
+++ my-express-app/api/index.ts
@@ -8,6 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({
     origin: "https://pizza-website-gold.vercel.app/",
+    // origin: "http://localhost:3000",
     credentials: true, 
 }));
 
