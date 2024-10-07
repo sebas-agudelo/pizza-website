eaaa39180ae87d5beb88b1710f3f63ab5b561f0e
Sebastian Agudelo <sebastianagudelo95@hotmail.com>
2024-10-07 12:01:28 +0200

    h3 css color red

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -5,25 +5,29 @@ import { IProducts } from './module/products';
 function App() {
     const [products, setProduct] = useState<IProducts[]>([]); // Initialize as an empty array
 
+    // Set this flag to switch between local and production URLs
+    const useLocalApi = true; // Change this to false to use the production URL
+
     useEffect(() => {
         fetProducts();
     }, []);
 
     const fetProducts = async () => {
         try {
-            // Use the appropriate API URL based on the environment
-            const apiUrl = process.env.NODE_ENV === 'production'
-                ? 'https://pizza-website-wona.vercel.app/'
-                : 'http://localhost:3001'
+            // Choose the API URL based on the useLocalApi flag
+            const apiUrl = useLocalApi
+                ? 'http://localhost:3001' // Local development URL
+                : 'https://pizza-website-wona.vercel.app/'; // Production URL
 
             const res = await fetch(apiUrl, {
                 method: "GET"
             });
+
             if (!res.ok) throw new Error('Network response was not ok');
 
             const data = await res.json();
             if (!data || data.length === 0) {
-                console.log('No products returned from the APII');
+                console.log('No products returned from the API');
                 setProduct([]); // Set empty array if no products
                 return;
             }
