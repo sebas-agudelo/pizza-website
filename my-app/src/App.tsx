2be928cda26718f6ecf103cfd8c0e89749b967fc
Sebastian Agudelo <sebastianagudelo95@hotmail.com>
2024-10-10 20:08:38 +0200

    added block images

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -18,33 +18,6 @@ function App() {
         const data = await res.json();
         setProduct(data)
         console.log(data);
-        
-
-
-        // try {
-        //     const apiUrl = useLocalApi
-        //     ? 'https://pizza-website-wona.vercel.app/'
-
-        //     : 'http://localhost:3001';  
-
-        //     const res = await fetch(apiUrl, {
-        //         method: "GET"
-        //     });
-
-        //     if (!res.ok) throw new Error('Network response was not ok');
-
-        //     const data = await res.json();
-        //     if (!data || data.length === 0) {
-        //         console.log('No products returned from the API');
-        //         setProduct([]);
-        //         return;
-        //     }
-
-        //     setProduct(data);
-        //     console.log(data);
-        // } catch (error) {
-        //     console.error('Error fetching products:', error);
-        // }
     };
 
     return (
