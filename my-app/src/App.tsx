bf9a0e2d09936748008e40e3f4bbae44c8d7457c
Sebastian Agudelo <sebastianagudelo95@hotmail.com>
2024-10-11 08:39:08 +0200

    ..

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -8,37 +8,9 @@ import Signup from './pages/Signup';
 import Products from './pages/Products';
 
 function App() {
-    const [products, setProduct] = useState<IProducts[]>([]); 
-
-    const useLocalApi = true;
-
-    useEffect(() => {
-        fetProducts();
-    }, []);
-
-    const fetProducts = async () => {
-
-        const res = await fetch('https://pizza-website-wona.vercel.app/');
-
-        const data = await res.json();
-        setProduct(data)
-        console.log(data);
-    };
 
     return (
         <div className="App">
-            <h1>Hello World</h1>
-            {products.length === 0 ? (
-                <div>Laddar.......</div>
-            ) : (
-                products.map((product) => (
-                    <div key={product.product_name}>
-                        <h3>Produktensnamn: {product.product_name}</h3>
-                        <h3>ProduktensPris: {product.product_price}kr</h3>
-                    </div>
-                ))
-            )}
-
             <BrowserRouter>
                 <Routes>
                     <Route path='/' element={<Home />}/>
--- my-app/src/pages/Products.tsx
+++ my-app/src/pages/Products.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { IProducts } from '../module/products';
 
 export default function Products() {
+  const [products, setProduct] = useState<IProducts[]>([]); 
+
+  useEffect(() => {
+    fetProducts();
+}, []);
+
+const fetProducts = async () => {
+
+    const res = await fetch('https://pizza-website-wona.vercel.app/');
+
+    const data = await res.json();
+    setProduct(data)
+    console.log(data);
+};
   return (
     <div>
       All products
+      {products.length === 0 ? (
+                <div>Laddar.......</div>
+            ) : (
+                products.map((product) => (
+                    <div key={product.product_name}>
+                        <h3>Produktensnamn: {product.product_name}</h3>
+                        <h3>ProduktensPris: {product.product_price}kr</h3>
+                    </div>
+                ))
+            )}
     </div>
   )
 }
