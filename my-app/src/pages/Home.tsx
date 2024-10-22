6093d0cb521506b3c112bdb75684e8f742f511dc
Sebastian Agudelo <sebastianagudelo95@hotmail.com>
2024-10-22 12:09:35 +0200

    Added a order button in the home page

--- my-app/src/pages/Home.tsx
+++ my-app/src/pages/Home.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Nav from '../components/Nav';
 import Footer from './Footer';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
     return (
         <div className='home'>
             <div className='hero-img'></div> {/* Bakgrundsbild */}
-
+            
+            <div className='btn-wrapper'>
+                <button><Link to={`/pizzor`}>Beställ</Link></button>
+            </div>
 
 
             <div className='bla'>
