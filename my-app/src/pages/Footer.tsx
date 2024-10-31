f83abfc9f2881647b479287e6568bcde73a4dc8e
Sebastian Agudelo <sebastianagudelo95@hotmail.com>
2024-10-31 11:44:36 +0100

    Footer completed (FrontEnd)

--- my-app/src/pages/Footer.tsx
+++ my-app/src/pages/Footer.tsx
@@ -4,31 +4,36 @@ import { Link } from 'react-router-dom'
 export default function Footer() {
   return (
     <footer>
-      {/* <div className='img-bkg'> 
-        <img src="pexels-arthurbrognoli-3343627.jpg" alt="" />
-      </div> */}
-      <div className='footer-order-wrapper'>
-        <h2>Gusto Pizza</h2>
+      <svg width="300" height="80">
+        <defs>
+          <path id="text-curve" d="M 50,108 A 75,50 0 1,1 250,100" />
+        </defs>
+        <text font-size="40" font-family="Arial, sans-serif">
+          <textPath href="#text-curve" startOffset="50%" text-anchor="middle">
+            Gusto Pizza
+          </textPath>
+        </text>
+      </svg>
 
+
+      <div className='app-actions-and-map'>
         <div className='order-btns'>
-          {/* <button>
-            <Link to={`/pizzor`}>Vår meny</Link>
-          </button> */}
-          {/* <button>
+          <button>
+            <Link to={`/pizzor`}>Avhämtning</Link>
+          </button>
+          <button>
             <Link to={`https://www.foodora.se/`}>Hemleverans</Link>
-          </button> */}
-
+          </button>
         </div>
 
-        <div className='footer-map'>
+        <div className='google-map'>
 
         </div>
       </div>
 
-      <div className='footer-info-wrapper'>
-
-        <div className='footer-contact-wrapper'>
+      <div className='footer-info-section'>
           <h2>Kontakt</h2>
+        <div className='contact-section'>
           <p>
             <a href="tel:0700000000">
               <i className="fa-solid fa-phone"></i>: 070-000-00-00
@@ -40,28 +45,29 @@ export default function Footer() {
             </a>
           </p>
         </div>
-        
-        <div className='open-times'>
+
+        <h2>Öppettider</h2>
+
+        <div className='open-times-section'>
           <p>Mån - Fre 10:30 - 21:00</p>
           <p>Lör - 11:30 - 22:00</p>
           <p>Sön - stängd</p>
         </div>
+
         {/* <div className='footer-logo-wrapper'>
-          <div className='logo'>
-            <img src="LOGO.png" alt="" />
-          </div>
-        </div> */}
-
-        <div className='social-media-icons'>
-
-          <div><i className="fa-brands fa-instagram"></i></div>
-          <div><i className="fa-brands fa-facebook-f"></i></div>
-          <div><i className="fa-brands fa-tiktok"></i></div>
-
-        </div>
-
+    <div className='logo'>
+      <img src="LOGO.png" alt="" />
+    </div>
+  </div> */}
       </div>
 
+      <div className='social-media-icons'>
+
+        <div><i className="fa-brands fa-instagram"></i></div>
+        <div><i className="fa-brands fa-facebook-f"></i></div>
+        <div><i className="fa-brands fa-tiktok"></i></div>
+
+      </div>
 
       <div className='copyright'>
         <p>© 2024 Gusto Pizza. All rights reserved.</p>
