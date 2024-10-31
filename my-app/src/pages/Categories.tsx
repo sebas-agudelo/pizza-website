247dd97462afb19f23f627e0686dae606e4ea3c5
Sebastian Agudelo <sebastianagudelo95@hotmail.com>
2024-10-31 14:18:58 +0100

    Category section completed

--- my-app/src/pages/Categories.tsx
+++ my-app/src/pages/Categories.tsx
@@ -25,18 +25,6 @@ export default function Categories() {
                     <img src="pexels-vanmalidate-784636.jpg" alt="" />
                 </div>
 
-                {/* <div className='SVG'>
-                    <svg width="300" height="80">
-                        <defs>
-                            <path id="text-curve" d="M 50,108 A 75,50 0 1,1 250,100" />
-                        </defs>
-                        <text font-size="40" font-family="Arial, sans-serif">
-                            <textPath href="#text-curve" startOffset="50%" text-anchor="middle">
-                                Gusto Pizza
-                            </textPath>
-                        </text>
-                    </svg>
-                </div> */}
                 <div className='div'>
                     <h2>VÅR MENY</h2>
                     <p>Upptäck en värld av smaker hos oss på GUSTO PIZZA. Här hittar du ett brett urval av pizzor tillagade med färska råvaror och en passion för hantverket. Oavsett om du är sugen på en klassisk Margherita, en smakrik Capricciosa eller en egenkomponerad favorit, så har vi något för dig. Välkommen att utforska vår meny och låt oss ta dig på en smakresa du sent kommer glömma!</p>
