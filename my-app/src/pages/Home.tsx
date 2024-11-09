74df62d00817a4f6214011decccf88f2bce4e287
Sebastian Agudelo <sebastianagudelo95@hotmail.com>
2024-11-09 14:51:22 +0100

    Home page completed

--- my-app/src/pages/Home.tsx
+++ my-app/src/pages/Home.tsx
@@ -137,13 +137,13 @@ export default function Home({ className }: HomeProps) {
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis saepe facere, officiis, repellendus eligendi doloribus ipsam quia itaque exercitationem ratione sapiente incidunt fugiat rem et laborum dolorem rerum quod. Harum.
                     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis eius ea eos alias eaque laboriosam at nostrum quis repellendus hic nam vero, quas cupiditate facilis in sunt sit. Eveniet, cumque?
                     
+                    
+                </p>
                     <div className='read-more-btn'>
                     <button>
                         <Link to={`about`}>READ MORE</Link>
                     </button>
                     </div>
-                    
-                </p>
 
             </section>
 
